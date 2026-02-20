d2e36d3d82efd469bde87a0aedbcece91fc84b25
thara-messeroux <tharamesseroux@gmail.com>
2026-02-19 23:03:00 -0500

    Install dependencies and configure environment setup

---
+++ app.js
@@ -0,0 +1,8 @@
+// Import prompt-sync
+const prompt = require('prompt-sync')();
+
+// Ask user a question
+const username = prompt("What is your name? ");
+
+// Print result
+console.log(`Hello ${username}`);
